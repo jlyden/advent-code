ec0b8c7d836115a49e43ebf7fb809ae296fce21b
jjl <jlyden1@alumni.nd.edu>
2022-02-05 14:34:50 -0500

    done with day seven part two

--- day-07/solution.day-seven.js
+++ day-07/solution.day-seven.js
@@ -1,27 +1,27 @@
 const fileUtils = require('../common/file-utils.js');
 const utils = require('../common/utils.js');
 
-console.log(getFuelCostToMoveCrabsIntoAlignment()); // Consistent Rate: 357353 moving to position 332
-//runTests();
+console.log(getFuelCostToMoveCrabsIntoAlignment(constantRate = true)); // Constant Rate: 357353 
+console.log(getFuelCostToMoveCrabsIntoAlignment(constantRate = false)); // Increasing Rate: 104822130
+runTests();
 
-function getFuelCostToMoveCrabsIntoAlignment(returnLimit = null) {
+function getFuelCostToMoveCrabsIntoAlignment(constantRate = true, returnLimit = null) {
   const crabPositions = fileUtils.getContents('day-07/input.txt', returnLimit, ',').map(num => parseInt(num));
-  return findLowestCostBruteForce(crabPositions);
+  return findLowestCostBruteForce(crabPositions, constantRate);
 }
 
-// This brute force method works, but with horrible efficiency
+// tested
+// Brute force method works, but with horrible efficiency
 // More data and/or greater range vastly increases computation time
-function findLowestCostBruteForce(crabPositions) {
-  const [ arrMin, arrMax ] = getMinMaxOfArray(crabPositions);
+function findLowestCostBruteForce(crabPositions, constantRate) {
+  const [ positionMin, positionMax ] = getMinMaxOfArray(crabPositions);
   const collectedCrabPositions = collectCrabPositions(crabPositions);
-  let allCosts = [];
-  for (let position = arrMin; position <= arrMax; position++) {
-    const costToMoveCrabs = getCostToMoveCrabs(collectedCrabPositions, position);
-    allCosts.push(costToMoveCrabs);
-  }
-  return getMinMaxOfArray(allCosts)[0];
+  const costPerDiff = constantRate ? null : getCostPerDiff(positionMin, positionMax);
+  let costsForAllPositions = getCostsToMoveCrabsToAllPositions(collectedCrabPositions, positionMin, positionMax, costPerDiff);
+  return getMinMaxOfArray(costsForAllPositions)[0];
 }
 
+// tested
 function collectCrabPositions(crabPositions) {
   let collectedCrabPositions = {};
   crabPositions.forEach(crabPosition => {
@@ -30,56 +30,66 @@ function collectCrabPositions(crabPositions) {
   return collectedCrabPositions;
 }
 
-function getCostToMoveCrabs(collectedCrabPositions, targetPosition) {
-  let costToMoveAll = 0;
-  for (const [position, count] of Object.entries(collectedCrabPositions)) {
-    const costToMoveCrabsAtPosition = getCostToMoveCrabsAtPositionConsistentRate(position, targetPosition, count)
-    costToMoveAll += costToMoveCrabsAtPosition
-  };
-  return costToMoveAll;
-}
-
-function getCostToMoveCrabsAtPositionConsistentRate(position, targetPosition, count) {
-  return Math.abs(parseInt(position) - targetPosition) * count;
-}
-
-function getCostToMoveCrabsAtPositionIncreasingRate(position, targetPosition, count) {
-  const diffBetweenPositions = Math.abs(parseInt(position) - targetPosition);
-  const costForOne = calculateCostIncreasingRate(diffBetweenPositions);
-  return costForOne * count;
-}
-
-function calculateCostIncreasingRate(diffBetweenPositions) {
-  if (!diffBetweenPositions) {
-    throw `Error: calculateCostIncreasingRate invalid param: ${JSON.stringify(diffBetweenPositions)}`;
+function getCostPerDiff(positionMin, positionMax) {
+  let costPerDiff = {};
+  for (let targetPosition = positionMin; targetPosition <= positionMax; targetPosition++) {
+    const diff = getDiffBetweenPositions(targetPosition, positionMax);
+    let cost = calculateCostIncreasingRate(diff);
+    costPerDiff[diff] = cost;
   }
-  const range = utils.range(1, diffBetweenPositions);
+  return costPerDiff;
+}
+
+// tested
+function calculateCostIncreasingRate(diff) {
+  if (diff < 0) {
+    throw `Error: calculateCostIncreasingRate invalid param: ${JSON.stringify(diff)}`;
+  }
+
+  if (diff === 0) {
+    return diff;
+  }
+  const range = utils.range(1, diff);
   return utils.sumArray(range);
 }
 
+function getCostsToMoveCrabsToAllPositions(collectedCrabPositions, positionMin, positionMax, costPerDiff) {
+  let costsForAllPositions = [];
+  for (let targetPosition = positionMin; targetPosition <= positionMax; targetPosition++) {
+    const costToMoveCrabsToPosition = getCostToMoveCrabsToPosition(collectedCrabPositions, targetPosition, costPerDiff);
+    costsForAllPositions.push(costToMoveCrabsToPosition);
+  }
+  return costsForAllPositions;
+}
+
+function getCostToMoveCrabsToPosition(collectedCrabPositions, targetPosition, costPerDiff) {
+  let costToMoveAllCrabs = 0;
+  for (const [crabPosition, count] of Object.entries(collectedCrabPositions)) {
+    let costToMoveOneCrab = getCostToMoveOneCrab(crabPosition, targetPosition, costPerDiff);
+//    console.log(`costToMoveOne * count: ${costToMoveOneCrab} * ${count}`);
+    costToMoveAllCrabs += (costToMoveOneCrab * count);
+  };
+  return costToMoveAllCrabs;
+}
+
+function getCostToMoveOneCrab(crabPosition, targetPosition, costPerDiff) {
+  const diff = getDiffBetweenPositions(crabPosition, targetPosition);
+//  console.log(`diffBtwnPositions: ${diff}`);
+  const ret = costPerDiff ? costPerDiff[diff] : diff;
+//  console.log(`getCostToMoveOneCrab ret: ${ret}`);
+  return ret;
+}
+
+function getDiffBetweenPositions(positionOne, positionTwo) {
+  return Math.abs(parseInt(positionOne) - positionTwo);
+}
+
 function getMinMaxOfArray(arr) {
+  if (!Array.isArray(arr)) {
+    throw `Error: getMinMaxOfArray called with non array: ${JSON.stringify(arr)}`;
+  }
   arr.sort((a,b) => a - b);
   return [ arr[0], arr[arr.length-1] ];
-}
-
-/* Attempts to figure out a smarter way to reach an answer -> to little avail */
-function getFuelCostToMoveCrabsIntoAlignment(returnLimit = null) {
-  const crabPositions = fileUtils.getContents('day-07/input.txt', returnLimit, ',').map(num => parseInt(num));
-  const collectedCrabPositions = collectCrabPositions(crabPositions);
-  const middlePosition = getMiddleOfRangeOfPositions(collectedCrabPositions);
-  const averageOfPositions = getAverageOfPositions(crabPositions);
-}
-
-function getMiddleOfRangeOfPositions(collectedCrabPositions) {
-  const positions = Object.keys(collectedCrabPositions);
-  const bottomOfRange = positions[0];
-  const topOfRange = positions[positions.length - 1];
-  const middle = Math.round((parseInt(topOfRange) - parseInt(bottomOfRange)) / 2);
-  return middle;
-}
-
-function getAverageOfPositions(crabPositions) {
-  return Math.round((utils.sumArray(crabPositions)) / crabPositions.length);
 }
 
 /***** TESTS *****/
@@ -111,11 +121,37 @@ function testCollectCrabPositions() {
 
 function testFindLowestCostBruteForce() {
   const crabPositions = [ 8, 1, 12, 20 ];
+  let constantRate = true;
   let expectedResult = 23;
-  let actualResult = findLowestCostBruteForce(crabPositions);
+  let actualResult = findLowestCostBruteForce(crabPositions, constantRate);
 
   if (!utils.objectsEqual(expectedResult, actualResult)) {
-    throw `Failed findLowestCostBruteForce. actualResult: ${JSON.stringify(actualResult)}`;
+    throw `Failed findLowestCostBruteForce constantRate true. actualResult: ${JSON.stringify(actualResult)}`;
+  }
+
+  constantRate = false;
+  expectedResult = 106;
+  actualResult = findLowestCostBruteForce(crabPositions, constantRate);
+
+  if (!utils.objectsEqual(expectedResult, actualResult)) {
+    throw `Failed findLowestCostBruteForce constantRate false. actualResult: ${JSON.stringify(actualResult)}`;
+  }
+
+  const crabPositionsExampleFromSite = [16,1,2,0,4,2,7,1,2,14];
+  constantRate = true;
+  expectedResult = 37;
+  actualResult = findLowestCostBruteForce(crabPositionsExampleFromSite, constantRate);
+
+  if (!utils.objectsEqual(expectedResult, actualResult)) {
+    throw `Failed findLowestCostBruteForce  example from site, constantRate true. actualResult: ${JSON.stringify(actualResult)}`;
+  }
+
+  constantRate = false;
+  expectedResult = 168;
+  actualResult = findLowestCostBruteForce(crabPositionsExampleFromSite, constantRate);
+
+  if (!utils.objectsEqual(expectedResult, actualResult)) {
+    throw `Failed findLowestCostBruteForce example from site, constantRate false. actualResult: ${JSON.stringify(actualResult)}`;
   }
 
   console.log('Completed run of testFindLowestCostBruteForce successfully')
@@ -146,6 +182,24 @@ function testCalculateCostIncreasingRate() {
     throw `Failed calculateCostIncreasingRate for ${diff}. actualResult: ${actualResult}`;
   }
 
-  console.log('Completed run of testCalculateCostIncreasingRate successfully')
+  diff = 0;
+  expectedResult = 0;
+  actualResult = calculateCostIncreasingRate(diff)
 
+  if (expectedResult !== actualResult) {
+    throw `Failed calculateCostIncreasingRate for ${diff}. actualResult: ${actualResult}`;
+  }
+
+  diff = -1;
+  const expectedError = 'Error: calculateCostIncreasingRate invalid param: -1';
+  try {
+    actualResult = calculateCostIncreasingRate(diff)
+  } catch (error) {
+    if (error !== expectedError) {
+      throw `Failed calculateCostIncreasingRate; wrong error message: ${error}`;
+    }
+  }
+
+
+  console.log('Completed run of testCalculateCostIncreasingRate successfully')
 }
